57b0d5d22efee841377df7e8225b080cb287ee1d
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2013-09-25 00:30:50 -0400

    Handle preferences

--- SmarterWifiManager/src/main/java/net/kismetwireless/android/smarterwifimanager/FragmentPrefs.java
+++ SmarterWifiManager/src/main/java/net/kismetwireless/android/smarterwifimanager/FragmentPrefs.java
@@ -1,18 +1,65 @@
 package net.kismetwireless.android.smarterwifimanager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
 import android.util.Log;
 
 /**
  * Created by dragorn on 9/24/13.
  */
-public class FragmentPrefs extends PreferenceFragment {
+public class FragmentPrefs extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+    ListPreference timeoutPref;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Log.d("smarter", "Loading prefs xml");
         addPreferencesFromResource(R.xml.main_prefs);
+
+        timeoutPref = (ListPreference) getPreferenceScreen().findPreference(getString(R.string.prefs_item_shutdowntime));
+
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+        setPrefsSummary();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        setPrefsSummary();
+
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        setPrefsSummary();
+    }
+
+    private void setPrefsSummary() {
+        int v = Integer.parseInt(timeoutPref.getValue());
+
+        String s;
+
+        try {
+            s = getResources().getStringArray(R.array.prefsTimeSecondString)[v] + "\n";
+        } catch (ArrayIndexOutOfBoundsException e) {
+            s = "";
+        }
+
+        timeoutPref.setSummary(s + getString(R.string.prefs_item_shutdowntime_explanation));
     }
 }
