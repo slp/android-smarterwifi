072283270c9a67a878af48dc385203d60f42a92e
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2013-09-25 01:18:22 -0400

    Prefs fixes to provide back arrow, use proper getentry

--- SmarterWifiManager/src/main/java/net/kismetwireless/android/smarterwifimanager/PrefsActivity.java
+++ SmarterWifiManager/src/main/java/net/kismetwireless/android/smarterwifimanager/PrefsActivity.java
@@ -1,7 +1,9 @@
 package net.kismetwireless.android.smarterwifimanager;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 /**
  * Created by dragorn on 9/24/13.
@@ -11,6 +13,20 @@ public class PrefsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ActionBar ab = getActionBar();
+
+        ab.setDisplayHomeAsUpEnabled(true);
+
         getFragmentManager().beginTransaction().replace(android.R.id.content, new FragmentPrefs()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
