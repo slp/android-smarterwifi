c638cf5e5547aa0ef31400d27fa61c3707c5d73a
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2013-10-02 01:41:56 -0400

    Start moving towards nav drawer

--- SmarterWifiManager/src/main/java/net/kismetwireless/android/smarterwifimanager/MainActivity.java
+++ SmarterWifiManager/src/main/java/net/kismetwireless/android/smarterwifimanager/MainActivity.java
@@ -6,18 +6,27 @@ import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.doomonafireball.betterpickers.timepicker.TimePickerBuilder;
 
@@ -34,10 +43,16 @@ public class MainActivity extends FragmentActivity {
 
     private static int PREFS_REQ = 1;
 
-    SmarterWifiServiceBinder serviceBinder;
-    SmarterPagerAdapter pagerAdapter;
-    ViewPager viewPager;
-    ActionBar actionBar;
+    private SmarterWifiServiceBinder serviceBinder;
+    private SmarterPagerAdapter pagerAdapter;
+    private ViewPager viewPager;
+    private ActionBar actionBar;
+
+    private ArrayList<Integer[]> drawerContent = new ArrayList<Integer[]>();
+
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -48,10 +63,44 @@ public class MainActivity extends FragmentActivity {
 
         serviceBinder = new SmarterWifiServiceBinder(this);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        drawerContent.add(new Integer[] {R.string.nav_ignore, R.string.nav_ignore_sub});
+        drawerContent.add(new Integer[] {R.string.nav_bluetooth, R.string.nav_bluetooth_sub});
+        drawerContent.add(new Integer[] {R.string.nav_time, R.string.nav_time_sub});
+
+        /*
+        drawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, drawerTitles));
+                */
+        drawerList.setAdapter(new DualDrawerListAdapter(this, R.layout.drawer_list_item, drawerContent));
+        // mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+
         actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        actionBar.setDisplayHomeAsUpEnabled(false);
+        // actionBar.setDisplayHomeAsUpEnabled(false);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(getTitle());
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(getTitle());
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        drawerLayout.setDrawerListener(drawerToggle);
 
         viewPager = (ViewPager) findViewById(R.id.pager);
 
@@ -124,6 +173,20 @@ public class MainActivity extends FragmentActivity {
             serviceBinder.doUnbindService();
     }
 
+      @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -133,6 +196,10 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+
         if (item.getItemId() == R.id.action_settings) {
             startActivityForResult(new Intent(MainActivity.this, PrefsActivity.class), PREFS_REQ);
             return true;
@@ -261,4 +328,41 @@ public class MainActivity extends FragmentActivity {
         }
     }
 
+    public class DualDrawerListAdapter extends ArrayAdapter<Integer[]> {
+        private int layoutResourceId;
+
+        public DualDrawerListAdapter(Context context, int textViewResourceId, ArrayList<Integer[]> items) {
+            super(context, textViewResourceId, items);
+            layoutResourceId = textViewResourceId;
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            try {
+                final Integer[] entry = getItem(position);
+
+                View v = null;
+
+                if (convertView == null) {
+                    LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                    v = inflater.inflate(layoutResourceId, null);
+                } else {
+                    v = convertView;
+                }
+
+                TextView text1 = (TextView) v.findViewById(R.id.text1);
+                TextView text2 = (TextView) v.findViewById(R.id.text2);
+
+                text1.setText(getString(entry[0]));
+                text2.setText(getString(entry[1]));
+
+                return v;
+            } catch (Exception ex) {
+                Log.e("smarter", "error", ex);
+                return null;
+            }
+        }
+    }
 }
